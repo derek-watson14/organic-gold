9bed552aa42f2f5a0ca38a35fed443e110672882
Derek Watson <derek.watson92@gmail.com>
2020-10-29 20:15:45 -0700

    added equipment list to studio page

--- src/pages/studio.js
+++ src/pages/studio.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import imageUrlBuilder from '@sanity/image-url'
 
 import client from "../sanity/client"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ColorTitle from "../components/colorTitle"
 
+const builder = imageUrlBuilder(client);
+
+function urlFor(source) {
+  return builder.image(source);
+}
 
 const Studio = () => {
   const [content, setContent] = useState(null);
@@ -15,7 +21,10 @@ const Studio = () => {
     const params = {};
 
     client.fetch(query, params).then(data => {
-      setContent(data[0]);
+      setContent({
+        ...data[0],
+        pageImageUrl: urlFor(data[0].pageImage).url(),
+      });
     })
   }, [])
 
@@ -33,10 +42,32 @@ const Studio = () => {
     }
   `)
 
+  console.log(content)
+
   return (
     <Layout navImage={navImage} fadeColor={"#F0843B"}>
       <SEO title={getData("tabTitle")} description={getData("metaDescription")} />
-      <ColorTitle text={getData("pageHeader")} marginBottom="50px" />
+      <div className="container">
+        <ColorTitle text={getData("pageHeader")} marginBottom="50px" />
+        <img style={{width: "100%", margin: 0 }} src={getData("pageImageUrl")} />
+      </div>
+      <div className="container listsContainer">
+        <h2 className="header-font">{getData("subheader")}</h2>
+        <div className="equipmentLists">
+          {getData("lists", []).map(item => {
+            return (
+            <div className="equipmentSection">
+              <h3>{item.listName}</h3>
+              <ul>
+                {item.listItem.map(subitem => {
+                  return <li>{subitem}</li>
+                })}
+              </ul>
+            </div>
+            )
+          })}
+        </div>
+      </div>
     </Layout>
   )
 }
