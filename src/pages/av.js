912007c52226c396397e501ecf41701715d1a576
Derek Watson <derek.watson92@gmail.com>
2020-11-17 20:02:17 -0800

    finished AV page of the website with styling

--- src/pages/av.js
+++ src/pages/av.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import ReactPlayer from "react-player"
 
 import client from "../sanity/client"
 import Layout from "../components/layout"
@@ -15,6 +16,7 @@ const AV = () => {
     const params = {};
 
     client.fetch(query, params).then(data => {
+      console.log(data[0])
       setContent(data[0]);
     })
   }, [])
@@ -37,9 +39,31 @@ const AV = () => {
     <Layout navImage={navImage} fadeColor={"#F8E100"}>
       <SEO title={getData("tabTitle")} description={getData("metaDescription")} />
       <div className="container">
-        <ColorTitle text={getData("pageHeader")} marginBottom="50px" />
+        <div className="av-text-container">
+          <ColorTitle text={getData("pageHeader")} marginBottom="10px" />
+          {getData("textContent", []).map((para, i) => <p key={i} className="page-p av-p">{para}</p>)}
+        </div>
         <div className="av-media-container">
-          <iframe className="av-youtube-player" src="https://www.youtube.com/embed/0trv6YOkWck" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div className="media-item-container">
+            <h3 className="media-header">Organic Gold on YouTube</h3>
+            <iframe 
+              className="av-youtube-player" 
+              src="https://www.youtube.com/embed/0trv6YOkWck" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+              allowFullScreen 
+            />
+          </div>
+          <div className="media-item-container">
+            <h3 className="media-header">Organic Gold on SoundCloud</h3>
+            <ReactPlayer
+              // https://www.npmjs.com/package/react-instagram-embed
+              className="av-soundcloud-playlist"
+              url="https://soundcloud.com/user-66808316"
+              width={"100%"}
+              height={425}
+            />
+          </div>
         </div>
       </div>
     </Layout>
