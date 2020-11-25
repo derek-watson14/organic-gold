330f2d1f985a1c64727b5c61835264f1c2a28eb4
Derek Watson <derek.watson92@gmail.com>
2020-11-25 13:12:59 -0800

    add poster property to video tags

--- src/pages/av.js
+++ src/pages/av.js
@@ -93,7 +93,7 @@ const AV = () => {
                 return (
                   <div key={post.id} className="post-container">
                     {post.isVideo 
-                      ? (<video controls playsInline preload="metadata">
+                      ? (<video controls playsInline preload="metadata" poster={post.imageUrl}>
                           <source src={post.videoUrl} type="video/mp4" />
                           Your browser doesn't support this video display.
                          </video>)
