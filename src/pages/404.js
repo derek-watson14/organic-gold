de44e7bb7293cdf666dc15f57fd7f7e5f323b735
Derek Watson <derek.watson92@gmail.com>
2020-11-26 14:15:35 -0800

    added 404 page and styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,39 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ColorTitle from "../components/colorTitle"
+import LinkButton from "../components/linkButton"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1 className="h-one">404: Not Found</h1>
-    <p>This page doesn't exsist!</p>
-  </Layout>
-)
 
+const NotFoundPage = () => {
+  const navImage = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "pattern.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1200, quality: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout navImage={navImage} fadeColor={"#B0C0A5"}>
+      <SEO title="404: Not found" />
+      <div className="container">
+        <div className="nf-container">
+          <ColorTitle text={"404: Page Not Found"} marginBottom="25px" />
+          <p className="page-p">Something went wrong! The page you were searching for doesn't exist.</p>
+          <div className="nf-button-container">
+            <LinkButton text={"home"} to={"/home"} />
+            <LinkButton text={"contact"} to={"/contact"} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 export default NotFoundPage
