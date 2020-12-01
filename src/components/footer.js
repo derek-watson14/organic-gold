eb12e24447cf508a803169d8854bd1f4a4cc8713
Derek Watson <derek.watson92@gmail.com>
2020-12-01 14:01:09 -0800

    added links to youtube, sc, insta posts, more styling of ig posts

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBandcamp, faFacebookSquare, faInstagram, faSoundcloud } from '@fortawesome/free-brands-svg-icons'
-import { Link } from "gatsby"
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBandcamp,
+  faFacebookSquare,
+  faInstagram,
+  faSoundcloud,
+} from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'gatsby';
 
-import client from "../sanity/client"
-import emptyContent, { emptyList } from "../helpers/emptyContent"
-
+import client from '../sanity/client';
+import emptyContent, { emptyList } from '../helpers/emptyContent';
 
 const Footer = () => {
   const [content, setContent] = useState({
@@ -17,46 +21,84 @@ const Footer = () => {
     const query = '*[_type == "pages" && pageName == "footer"]';
     const params = {};
 
-    client.fetch(query, params).then(data => {
+    client.fetch(query, params).then((data) => {
       setContent(data[0]);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
-    <footer className="footer-container">
-      <div className="footer-connect text-font">
-        <h3 className="header-font">{content.pageHeader}</h3>
-        {content.lists[0].items.map((item, index) => <p key={index}>{item}</p>)}
-        <div className="icon-container">
-          <a href="hhttps://www.facebook.com/OrganicGoldMusic" rel="noreferrer" target="_blank" title="Facebook">
-            <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="social-icon" />
+    <footer className='footer-container'>
+      <div className='footer-connect text-font'>
+        <h3 className='header-font'>{content.pageHeader}</h3>
+        {content.lists[0].items.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+        <div className='icon-container'>
+          <a
+            href='hhttps://www.facebook.com/OrganicGoldMusic'
+            rel='noreferrer'
+            target='_blank'
+            title='Facebook'
+          >
+            <FontAwesomeIcon
+              icon={faFacebookSquare}
+              size='2x'
+              className='social-icon'
+            />
           </a>
-          <a href="https://www.instagram.com/organicgoldmusic/" rel="noreferrer" target="_blank" title="Instagram">
-            <FontAwesomeIcon icon={faInstagram} size="2x" className="social-icon" />
+          <a
+            href='https://www.instagram.com/organicgoldmusic/'
+            rel='noreferrer'
+            target='_blank'
+            title='Instagram'
+          >
+            <FontAwesomeIcon
+              icon={faInstagram}
+              size='2x'
+              className='social-icon'
+            />
           </a>
-          <a href="https://organicgold.bandcamp.com/" rel="noreferrer" target="_blank" title="Bandcamp">
-            <FontAwesomeIcon icon={faBandcamp} size="2x" className="social-icon" />
+          <a
+            href='https://organicgold.bandcamp.com/'
+            rel='noreferrer'
+            target='_blank'
+            title='Bandcamp'
+          >
+            <FontAwesomeIcon
+              icon={faBandcamp}
+              size='2x'
+              className='social-icon'
+            />
           </a>
-          <a href="https://soundcloud.com/user-66808316" rel="noreferrer" target="_blank" title="SoundCloud">
-            <FontAwesomeIcon icon={faSoundcloud} size="2x" className="social-icon" />
+          <a
+            href='https://soundcloud.com/user-66808316'
+            rel='noreferrer'
+            target='_blank'
+            title='SoundCloud'
+          >
+            <FontAwesomeIcon
+              icon={faSoundcloud}
+              size='2x'
+              className='social-icon'
+            />
           </a>
         </div>
         <p>&copy; Organic Gold {new Date().getFullYear()}</p>
       </div>
-      <div className="footer-spacer"></div>
-      <div className="footer-navigate">
-        <h3 className="header-font">Navigate</h3>
-        <div className="footer-link-container text-font">
-          <Link to="/">Home &gt;</Link>
-          <Link to="/band">The Band &gt;</Link>
-          <Link to="/studio">The Studio &gt;</Link>
-          <Link to="/av">A/V &gt;</Link>
-          <Link to="/shows">Shows &gt;</Link>
-          <Link to="/contact">Contact &gt;</Link>
+      <div className='footer-spacer'></div>
+      <div className='footer-navigate'>
+        <h3 className='header-font'>Navigate</h3>
+        <div className='footer-link-container text-font'>
+          <Link to='/'>Home &gt;</Link>
+          <Link to='/band'>The Band &gt;</Link>
+          <Link to='/studio'>The Studio &gt;</Link>
+          <Link to='/av'>A/V &gt;</Link>
+          <Link to='/shows'>Shows &gt;</Link>
+          <Link to='/contact'>Contact &gt;</Link>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/av.js
+++ src/pages/av.js
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import ReactPlayer from "react-player"
-import axios from "axios"
+import React, { useState, useEffect } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import ReactPlayer from 'react-player';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faYoutube,
+  faSoundcloud,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
-import client from "../sanity/client"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import ColorTitle from "../components/colorTitle"
-import emptyContent from "../helpers/emptyContent"
-
+import client from '../sanity/client';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import ColorTitle from '../components/colorTitle';
+import emptyContent from '../helpers/emptyContent';
 
 const AV = () => {
   const [content, setContent] = useState(emptyContent);
@@ -18,31 +23,33 @@ const AV = () => {
     const query = '*[_type == "pages" && pageName == "av"]';
     const params = {};
 
-    client.fetch(query, params).then(data => {
+    client.fetch(query, params).then((data) => {
       setContent(data[0]);
-    })
-  }, [])
+    });
+  }, []);
 
   useEffect(() => {
-    const igQueryString = "https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={%22id%22:%2222186333894%22,%22first%22:6}";
-    axios.get(igQueryString)
-      .then(res => {
-        const postArray = res.data.data.user.edge_owner_to_timeline_media.edges;
-        const posts = postArray.map((postData) => {
-          const post = postData.node;
-          return {
-            id: post.id,
-            imageUrl: post.display_url,
-            caption: post.edge_media_to_caption.edges[0].node.text,
-            location: post.location,
-            likes: post.edge_media_preview_like.count,
-            isVideo: post.is_video,
-            videoUrl: post.is_video && post.video_url,
-          }
-        })
-        setInstaPosts(posts);
-      })
-  }, [])
+    const igQueryString =
+      'https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={%22id%22:%2222186333894%22,%22first%22:6}';
+    axios.get(igQueryString).then((res) => {
+      const postArray = res.data.data.user.edge_owner_to_timeline_media.edges;
+      console.log(postArray);
+      const posts = postArray.map((postData) => {
+        const post = postData.node;
+        return {
+          id: post.id,
+          imageUrl: post.display_url,
+          caption: post.edge_media_to_caption.edges[0].node.text,
+          location: post.location,
+          likes: post.edge_media_preview_like.count,
+          isVideo: post.is_video,
+          videoUrl: post.is_video && post.video_url,
+          postUrl: `https://www.instagram.com/p/${post.shortcode}/`,
+        };
+      });
+      setInstaPosts(posts);
+    });
+  }, []);
 
   const concatCaption = (caption) => {
     if (caption.length > 247) {
@@ -50,7 +57,7 @@ const AV = () => {
     } else {
       return caption;
     }
-  }
+  };
 
   const navImage = useStaticQuery(graphql`
     query {
@@ -62,63 +69,119 @@ const AV = () => {
         }
       }
     }
-  `)
+  `);
 
   return (
-    <Layout navImage={navImage} fadeColor={"#F8E100"}>
+    <Layout navImage={navImage} fadeColor={'#F8E100'}>
       <SEO title={content.tabTitle} description={content.metaDescription} />
-      <div className="container">
-        <div className="av-text-container">
-          <ColorTitle text={content.pageHeader} marginBottom="10px" />
-          {content.textContent.map((para, i) => <p key={i} className="page-p av-p">{para}</p>)}
+      <div className='container'>
+        <div className='av-text-container'>
+          <ColorTitle text={content.pageHeader} marginBottom='10px' />
+          {content.textContent.map((para, i) => (
+            <p key={i} className='page-p av-p'>
+              {para}
+            </p>
+          ))}
         </div>
-        <div className="av-media-container">
-          <div className="av-youtube-player">
-            <h3 className="media-header">{content.externalMedia.youTubeVideo.header}</h3>
-            <iframe 
+        <div className='av-media-container'>
+          <div className='av-youtube-player'>
+            <a
+              href='https://www.youtube.com/channel/UCxH2v_DVDTZD_mGCvlz-b3g/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <h3 className='media-header'>
+                {content.externalMedia.youTubeVideo.header}
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  size='lg'
+                  className='media-head-icon'
+                />
+              </h3>
+            </a>
+            <iframe
               src={content.externalMedia.youTubeVideo.link}
-              frameBorder="0" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-              allowFullScreen 
-              title="YouTubePlayer"
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+              allowFullScreen
+              title='YouTubePlayer'
             />
           </div>
-          <div className="av-soundcloud-playlist">
-            <h3 className="media-header">{content.externalMedia.scPlayer.header}</h3>
+          <div className='av-soundcloud-playlist'>
+            <a
+              href='https://soundcloud.com/user-66808316/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <h3 className='media-header'>
+                {content.externalMedia.scPlayer.header}
+                <FontAwesomeIcon
+                  icon={faSoundcloud}
+                  size='lg'
+                  className='media-head-icon'
+                />
+              </h3>
+            </a>
             <ReactPlayer
               url={content.externalMedia.scPlayer.link}
-              width={"100%"}
+              width={'100%'}
               height={425}
             />
           </div>
-          <div className="av-instagram">
-            <a href="https://www.instagram.com/organicgoldmusic/" target="_blank" rel="noreferrer">
-              <h3 className="media-header">@organicgoldmusic on Instagram</h3>
+          <div className='av-instagram'>
+            <a
+              href='https://www.instagram.com/organicgoldmusic/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <h3 className='media-header'>
+                <span className='ig-username'>@organicgoldmusic</span> on
+                Instagram
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  size='1x'
+                  className='ig-icon'
+                />
+              </h3>
             </a>
             <hr />
-            <div className="insta-grid">
-              {instaPosts.map(post => {
+            <div className='insta-grid'>
+              {instaPosts.map((post) => {
                 return (
-                  <div key={post.id} className="post-container">
-                    {post.isVideo 
-                      ? (<video controls poster={post.imageUrl}>
-                          <source src={post.videoUrl} type="video/mp4" />
-                          Your browser doesn't support this video display.
-                         </video>)
-                      : <img src={post.imageUrl} alt={post.caption} />
-                    }
-                    <div className={post.isVideo ? "hover-overlay video-o" : "hover-overlay"}>
-                      <h4 className="post-caption">{post.isVideo ? concatCaption(post.caption) : post.caption}</h4>
+                  <div key={post.id} className='post-container'>
+                    {post.isVideo ? (
+                      <video controls poster={post.imageUrl}>
+                        <source src={post.videoUrl} type='video/mp4' />
+                        Your browser doesn't support this video display.
+                      </video>
+                    ) : (
+                      <img src={post.imageUrl} alt={post.caption} />
+                    )}
+                    <div
+                      className={
+                        post.isVideo ? 'hover-overlay video-o' : 'hover-overlay'
+                      }
+                    >
+                      <h4 className='post-caption'>
+                        {post.isVideo
+                          ? concatCaption(post.caption)
+                          : post.caption}
+                      </h4>
                     </div>
+                    <a href={post.postUrl} target='_blank' rel='noreferrer'>
+                      <h3 className='ig-post-link'>
+                        View on Instagram &gt;&gt;
+                      </h3>
+                    </a>
                   </div>
-                )
+                );
               })}
             </div>
           </div>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default AV
+export default AV;
